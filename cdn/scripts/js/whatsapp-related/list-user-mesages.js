af6c78a9c49d0bc8c5d60a5bb160a6b9a7936a25
FXMeeT <155169014+FXMeeT@users.noreply.github.com>
2026-06-01 19:47:28 +0800

    Update list-user-mesages.js

--- cdn/scripts/js/whatsapp-related/list-user-mesages.js
+++ cdn/scripts/js/whatsapp-related/list-user-mesages.js
@@ -3,9 +3,15 @@ const matches = [...document.querySelectorAll('div[data-testid^="conv-msg-"]')]
         el.querySelector('[data-testid="msg-container"]') &&
         el.querySelector('[data-testid="msg-meta"]') &&
         (
-            el.querySelector('[data-testid="selectable-text"]') || // text message
-            el.querySelector('[data-testid="image-thumb"]') || // image message
-            el.querySelector('[data-testid="image-caption selectable-text"]') // image caption
+            // text messages
+            el.querySelector('[data-testid="selectable-text"]') ||
+
+            // image messages
+            el.querySelector('[data-testid="image-thumb"]') ||
+            el.querySelector('[data-testid="image-caption selectable-text"]') ||
+
+            // video messages (NEW)
+            el.querySelector('[data-testid="video-content"]')
         )
     );
 
